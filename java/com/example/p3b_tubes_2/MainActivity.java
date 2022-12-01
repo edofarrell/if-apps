91824e20327ebf6b280538b99a846e2dd1818adb
edofarrell <edo.farrell17@gmail.com>
2022-12-01 14:05:55 +0700

    Add binding on MainActivity

--- java/com/example/p3b_tubes_2/MainActivity.java
+++ java/com/example/p3b_tubes_2/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.p3b_tubes_2.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(this.binding.getRoot());
     }
 }
