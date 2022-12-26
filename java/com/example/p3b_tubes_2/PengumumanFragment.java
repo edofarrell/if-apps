a6ca36043a3af93f5999f3d67fcbcef741afbe94
WilliamNehemia6182001041 <6182001041@student.unpar.ac.id>
2022-12-26 15:05:05 +0700

    membuat halaman pengumuman

--- java/com/example/p3b_tubes_2/PengumumanFragment.java
+++ java/com/example/p3b_tubes_2/PengumumanFragment.java
@@ -1,21 +1,36 @@
 package com.example.p3b_tubes_2;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import android.widget.Button;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.PopupMenu;
 import androidx.fragment.app.Fragment;
 
 import com.example.p3b_tubes_2.databinding.FragmentPengumumanBinding;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
-public class PengumumanFragment extends Fragment{
+import java.util.ArrayList;
+import java.util.List;
+
+public class PengumumanFragment extends Fragment {
 
     private FragmentPengumumanBinding binding;
+    private ChipGroup chipGroup;
 
-    private PengumumanFragment(){}
+    private PengumumanFragment() {
+    }
 
     public static PengumumanFragment newInstance() {
         Bundle args = new Bundle();
@@ -28,6 +43,55 @@ public class PengumumanFragment extends Fragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         this.binding = FragmentPengumumanBinding.inflate(inflater);
+
+        binding.ivFilter.setOnClickListener(this::onClick);
+        chipGroup = binding.chipGrup;
         return this.binding.getRoot();
     }
+
+
+    public void onClick(View view) {
+        if (view == binding.ivFilter) {
+            PopupMenu popupMenu = new PopupMenu(getContext(), binding.ivFilter);
+
+
+            //nambah pop up menu (nanti diisi dari yg api tag tag nya)
+            popupMenu.getMenu().add(1, 1, 1, "slot1");
+            popupMenu.getMenu().add(1, 2, 2, "slot2");
+            popupMenu.getMenu().add(1, 3, 3, "slot3");
+            popupMenu.getMenuInflater().inflate(R.menu.popup_menu, popupMenu.getMenu());
+            popupMenu.show();
+
+            popupMenu.setOnMenuItemClickListener(this::onOptionsItemSelected);
+        }
+    }
+
+
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        CharSequence idS = item.getTitle();
+        String text = (String) item.getTitle();
+        Chip chip = new Chip(getContext());
+        chip.setText(text);
+
+        chip.setCloseIconVisible(true);
+        chip.setTextColor(getResources().getColor(R.color.black));
+        chip.setOnCloseIconClickListener(this::OnCloseIconClick);
+
+
+        chipGroup.addView(chip);
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    public boolean OnCloseIconClick(View view){
+        Chip chip = (Chip) view;
+        chipGroup.removeView(view);
+        return true;
+    }
+
+
 }
